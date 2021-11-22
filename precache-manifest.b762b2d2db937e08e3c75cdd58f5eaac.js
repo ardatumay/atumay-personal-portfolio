44831a3d4f930ad24083cfe3324f7ee0107ec03c
Arda Tümay <arda.tumay@tedu.edu.tr>
2021-11-22 20:14:22 +0300

    build

---
+++ precache-manifest.b762b2d2db937e08e3c75cdd58f5eaac.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11bb9df1ba3e6013ced82c1c5bbccec",
+    "url": "/atumay-personal-portfolio/index.html"
+  },
+  {
+    "revision": "e5b499366714adc4d2b0",
+    "url": "/atumay-personal-portfolio/static/css/3.577476ef.chunk.css"
+  },
+  {
+    "revision": "9604a37737fbe4370a68",
+    "url": "/atumay-personal-portfolio/static/css/4.839f9e29.chunk.css"
+  },
+  {
+    "revision": "2704ecda30aa8ca4c78e",
+    "url": "/atumay-personal-portfolio/static/css/main.99ded1ed.chunk.css"
+  },
+  {
+    "revision": "6020959f017aab692402",
+    "url": "/atumay-personal-portfolio/static/js/2.be2591db.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/atumay-personal-portfolio/static/js/2.be2591db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b499366714adc4d2b0",
+    "url": "/atumay-personal-portfolio/static/js/3.7ac456fa.chunk.js"
+  },
+  {
+    "revision": "9604a37737fbe4370a68",
+    "url": "/atumay-personal-portfolio/static/js/4.011c95b8.chunk.js"
+  },
+  {
+    "revision": "2704ecda30aa8ca4c78e",
+    "url": "/atumay-personal-portfolio/static/js/main.84838098.chunk.js"
+  },
+  {
+    "revision": "c71196780dd44fa7be0d",
+    "url": "/atumay-personal-portfolio/static/js/runtime-main.84ea7e49.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/atumay-personal-portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/atumay-personal-portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "0e894c5fc670090e0acf4161700596f1",
+    "url": "/atumay-personal-portfolio/static/media/TEDUlogoEn.0e894c5f.gif"
+  },
+  {
+    "revision": "5f525c72517d76be554297a4497f226b",
+    "url": "/atumay-personal-portfolio/static/media/anatomy-platform.5f525c72.jpg"
+  },
+  {
+    "revision": "0393531d3bfc933b1e5671a1b8175690",
+    "url": "/atumay-personal-portfolio/static/media/bilkent-amblem.0393531d.png"
+  },
+  {
+    "revision": "a370f618ad76e4744345e948e3ececb1",
+    "url": "/atumay-personal-portfolio/static/media/bodrumextra.a370f618.png"
+  },
+  {
+    "revision": "1aed41fc506df4de52c267774ed5a39a",
+    "url": "/atumay-personal-portfolio/static/media/cagdas.1aed41fc.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/atumay-personal-portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "c9cac021b5c53787a0ae7cccec795b49",
+    "url": "/atumay-personal-portfolio/static/media/cyberparklogo.c9cac021.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/atumay-personal-portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "3e2c41272e551931740548790e335afd",
+    "url": "/atumay-personal-portfolio/static/media/logo-ceo-cube.3e2c4127.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/atumay-personal-portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "2795b84f44f0dbf63ef6bcd87381f163",
+    "url": "/atumay-personal-portfolio/static/media/milsoft.2795b84f.jpg"
+  },
+  {
+    "revision": "bab2508ef0750467382aa59cd7047d92",
+    "url": "/atumay-personal-portfolio/static/media/olimobile.bab2508e.png"
+  },
+  {
+    "revision": "679dad3d01136718a05eccbae677c82a",
+    "url": "/atumay-personal-portfolio/static/media/safebuslogo.679dad3d.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/atumay-personal-portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/atumay-personal-portfolio/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "15037c0f43a7b68ae39ca57e81d61357",
+    "url": "/atumay-personal-portfolio/static/media/teduapp.15037c0f.png"
+  },
+  {
+    "revision": "8a7c2c8c6127e308a466ff5b38e381cb",
+    "url": "/atumay-personal-portfolio/static/media/visualern.8a7c2c8c.png"
+  }
+]);
